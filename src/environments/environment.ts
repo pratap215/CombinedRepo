505bff9af6075eda49f2aa783f905b27d1de4525
pratap215 <styleepop@gmail.com>
2022-02-07 14:19:49 +0530

    new api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     // environmentName:'dwpstage',
 
     regionSpecifier: "",
-    translatorApiKey: "3f272905281e48cc9e539d881729dab2",
+    translatorApiKey: "583852e1369d4f0caf4928a6457bfcef",
 
   }
 
